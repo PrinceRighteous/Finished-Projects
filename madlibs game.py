a9e048dd1016510ce50ef0ca95f2d5249deaae03
prince <zig_zag_zig24@yahoo.com>
2023-05-04 18:36:47 -0400

    Add files via upload

---
+++ madlibs game.py
@@ -0,0 +1,30 @@
+madlibs = [input("Enter a color: "), input("please enter a color: "),input("Enter a noun: ")]
+
+print('Roses are ' + madlibs[0])
+print('violets are ' + madlibs[1])
+print('you love ' + madlibs[2])
+print('Hopefully ' + madlibs[2] + ' loves you tooooo')
+
+
+print("created by Eijah Thomas")
+
+
+
+
+
+
+madlibs2 = [input("please input a noun: "), input("please input a noun: "), input("DO FOR?: "), input("please input a noun: "), input("LAZINESS CREATES?: "),
+            input("KEEP UP?: "), input("AND?: "), input("Please input a noun: ")]
+
+
+print("Heaven is ".title() + madlibs2[0])
+print("Hell is ".title() + madlibs2[1])
+print("you must do for ".title() + madlibs2[2])
+print("to achieve the heaven you ".title() + madlibs2[3])
+print("Sleep and laziness creates ".title() + madlibs2[4])
+print("keep up ".title() + madlibs2[5] + " and " + madlibs2[6])
+print("surely you will be ".title() + madlibs2[7])
+
+
+
+print("created by Elijah Thomas".title())
